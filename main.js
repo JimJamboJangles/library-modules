6607559941a523957280d1f659e57e5c2e6d637d
James DeVore <jdevore4592@gmail.com>
2018-10-30 17:46:58 -0400

    Made Library and Books module

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-// write your code here to make the tests pass
+var Library = function() {
+  var books = []
+
+  var addBook = function(book) {
+    books.push(book);
+
+  }
+  var checkOutBook = function(book) {
+
+    if (Library[book]) {
+      book[checkedOut] = true;
+    } else {
+      console.log("Error");
+    }
+  }
+  var returnBook = function(book) {
+
+  }
+  return {
+    addBook: addBook,
+    returnBook: returnBook,
+    checkOutBook: checkOutBook
+  }
+};
+
+var Book = function() {
+  var attributes {
+
+  }
+  var getAttribute = function(attribute) {
+    if (Book[attribute]) {
+      return Book[attribute];
+    }
+  };
+  var setAttribute = function(attribute) {
+    if (Book[attribute]) {
+      Book[attribute] = attribute;
+    }
+  };
+
+  return {
+    getAttribute: getAttribute,
+    setAttribute: setAttribute
+  }
+};
